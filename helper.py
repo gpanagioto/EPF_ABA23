df63ce0d9aa9828c498788557a0970994b9954f8
elenamp28 <elenamp28@gmail.com>
2023-04-14 01:31:47 +0200

    update models

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn import metrics
 import datetime as dt
 
 def read_our_data(file_name):
@@ -10,22 +11,25 @@ def read_our_data(file_name):
     cols_check = ['Timestamp', 'Date']
     for col in cols_check:
         if col in df.columns:
-            df[col] = pd.to_datetime([col])
+            df[col] = pd.to_datetime(df[col])
     return df
 
-def split_timeseries(df, train_s, train_e, test_s, test_e): # format of dates has to be YYYY-mm-dd
-    if (train_e <= train_s):
-        print('Incoherent dates for the TRAIN set!')
-    elif (test_e <= test_s):
-        print('Incoherent dates for the TEST set!')
-    else:
-        X_train = df.loc[train_s:train_e]
-        X_test = df.loc[test_s:test_e]
+# def split_timeseries(df, train_s, train_e, test_s, test_e): # format of dates has to be YYYY-mm-dd
+#     if (train_e <= train_s):
+#         print('Incoherent dates for the TRAIN set!')
+#     elif (test_e <= test_s):
+#         print('Incoherent dates for the TEST set!')
+#     else:
+#         X_train = df.loc[train_s:train_e]
+#         X_test = df.loc[test_s:test_e]
         
-        return X_train, X_test
+#         return X_train, X_test
     
-def split_timeseries1(df, train_start, cnt, method, perc = 0.85): # train_start array of dates; formatted YYYY-mm-dd
-    
+def split_timeseries(df, train_start, cnt, method, perc = 0.85): # train_start array of dates; formatted YYYY-mm-dd
+    # method {0 - moving blocks, 1 - from the begging}
+    if 'Timestamp' in df.columns:
+        df.set_index('Timestamp', inplace = True)
+
     if cnt == len(train_start)-1:
         end=df['Date'].max()
     else:
@@ -48,13 +52,24 @@ def split_timeseries1(df, train_start, cnt, method, perc = 0.85): # train_start 
     X_test = df.loc[test_start:end]
        
     return X_train, X_test
-    
+
+def get_feature_target(df, features, target):
+    df.reset_index(inplace = True)
+    if 'Timestamp' in df.columns:
+        df.drop(['Timestamp'], axis = 1)
+    X = df[features]
+    y = df[target]
+
+    return X, y
+
 def standardize(train_set, test_set, cols):
-    mu = train_set[cols].mean()
-    std = train_set[cols].std()
+    mu = train_set[cols].mean(axis = 0)
+    std = train_set[cols].std(axis = 0)
     
-    train_set_std = (train_set - mu) / std
-    test_set_std = (test_set - mu) / std
+    train_set_std = train_set.copy()
+    test_set_std = test_set.cop()
+    train_set_std[cols] = (train_set[cols] - mu) / std
+    test_set_std[cols] = (test_set[cols] - mu) / std
     
     return train_set_std, test_set_std
 
