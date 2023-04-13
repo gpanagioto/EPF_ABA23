6165f45b2bb440c9ac8b4cf7235282d49f792569
elenamp28 <elenamp28@gmail.com>
2023-04-13 22:09:17 +0200

    update preparation

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import TimeSeriesSplit
+import datetime as dt
 
 def split_timeseries(df, train_s, train_e, test_s, test_e): # format of dates has to be YYYY-mm-dd
     if (train_e <= train_s):
@@ -13,6 +14,31 @@ def split_timeseries(df, train_s, train_e, test_s, test_e): # format of dates ha
         X_test = df.loc[test_s:test_e]
         
         return X_train, X_test
+    
+def split_timeseries1(df, train_start, cnt, method, perc = 0.85): # train_start array of dates; formatted YYYY-mm-dd
+    
+    if cnt == len(train_start)-1:
+        end=df['Date'].max()
+    else:
+        end = train_start[cnt +1] - dt.timedelta(days=1)
+
+    if method == 0:
+        start = train_start[cnt]
+        delta=(end-start).days
+        train_end = start + pd.DateOffset(days=round(perc*delta,0))
+        test_start = train_end + pd.DateOffset(days=1)
+
+    elif method == 1:
+        start = train_start[0]
+        delta=(end-start).days
+        train_end = end - pd.DateOffset(days=91)
+        test_start = train_end + pd.DateOffset(days=1)
+    
+
+    X_train = df.loc[start:train_end]
+    X_test = df.loc[test_start:end]
+       
+    return X_train, X_test
     
 def standardize(train_set, test_set, cols):
     mu = train_set[cols].mean()
