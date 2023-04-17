fb3e74b2f6ae7e63c5e297742c8f11c9bc82a975
elenamp28 <elenamp28@gmail.com>
2023-04-17 18:00:39 +0200

    update merging

---
+++ helper.py
@@ -0,0 +1,9 @@
+def remove_utc(col_name, # str: name of the column that contains the object to convert to timestamp
+               tz_offset, # int: timezone offset. E.g., CET = +1
+               df # dataframe: contains all the info
+              ):
+    df['Timestamp'] = pd.to_datetime(df[col_name], format = '%Y %m %d %H:%M:%S',utc = True)
+    df['Timestamp'] = (df['Timestamp'] + dt.timedelta(hours = tz_offset)).dt.tz_localize(None)
+    df.drop([col_name], axis = 1, inplace = True) # drop the column
+    df.set_index('Timestamp', inplace = True) # set column 'Timestamp' as index
+    return df
