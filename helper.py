bf8125f506c7736d28f99580bd7bf269f919f1d1
unknown <giannis379k@gmail.com>
2023-04-21 22:35:21 +0200

    update models

--- helper.py
+++ helper.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn import metrics
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor
 from sklearn.model_selection import RandomizedSearchCV, TimeSeriesSplit
 from sklearn.linear_model import LinearRegression
-
+from sklearn.neural_network import MLPRegressor 
 import datetime as dt
 import seaborn as sns
 
@@ -27,7 +27,20 @@ def lag_df(df:pd.DataFrame(), col:str, lags:list) -> pd.DataFrame():
     for lag in lags:
         new_col = str(col) + '-lag' + str(lag)
         df_lagged[new_col] = df_lagged[col].shift(lag)
+    
     return df_lagged
+
+
+def drop_lag_df(df:pd.DataFrame(), new_col:str, lags:list) -> pd.DataFrame():
+    df_lagged = df.copy()
+    for lag in lags:
+        col = str(new_col) + '-lag' + str(lag)
+        print(col, df_lagged[df_lagged[col].isna()]['Date'].unique())
+        idx_drop=(df_lagged[df_lagged[col].isna()]['Date'].unique())
+        df_lagged.drop(df_lagged[df_lagged['Date'].isin(idx_drop)].index, inplace = True)
+    return df_lagged
+
+
 
 def split_timeseries(df:pd.DataFrame(), train_start:list, cnt:int, method:int, perc = 0.85) -> pd.DataFrame(): # train_start array of dates; formatted YYYY-mm-dd
     # method {0 - moving blocks, 1 - from the begging}
@@ -37,7 +50,7 @@ def split_timeseries(df:pd.DataFrame(), train_start:list, cnt:int, method:int, p
         df.set_index('Timestamp', inplace = True)
 
     if cnt == len(train_start)-1:
-        end=df['Date'].max() + dt.timedelta(hours = 23)
+        end=pd.to_datetime(df['Date'].max()) + dt.timedelta(hours = 23)
 
     else:
         end = train_start[cnt +1] - dt.timedelta(hours = 1)
@@ -128,9 +141,17 @@ def build_xgb(x_train, y_train, x_test):
     
     model_= xgb
     ypred = model_.predict(x_test)
-    params = model_.params()
 
-    return ypred, params
+    return ypred, model_
+
+def build_gb(x_train, y_train, x_test):
+    xgb = GradientBoostingRegressor(seed = 42)
+    xgb.fit(x_train, y_train)
+    
+    model_= xgb
+    ypred = model_.predict(x_test)
+
+    return ypred, model_
 
 def nn (x_train, y_train, x_test, y_test):
 
@@ -138,7 +159,7 @@ def nn (x_train, y_train, x_test, y_test):
     ypred = model_.predict(x_test)
     return ypred, model_
 
-def run_model(model_type, df, k_folds, split_method, train_start, features, target, cols_std, pred_window = []): # argument timeframe is necessary to choose baseline
+def run_model(model_type, df, k_folds, split_method, train_start, features, target, cols_std, std=True, pred_window = []): # argument timeframe is necessary to choose baseline
     # model_type: lr (linear regression), ma (moving average), rf (random forest), xgb (XGBoost), nn (neural network)
 
     if model_type == 'ma':
@@ -157,7 +178,10 @@ def run_model(model_type, df, k_folds, split_method, train_start, features, targ
             X_test, y_test = get_feature_target(test_set, features, target)
 
             # standardize
-            X_train_std, X_test_std = standardize(X_train, X_test, cols_std)
+            if std==True:
+                X_train_std, X_test_std = standardize(X_train, X_test, cols_std)
+            else:
+                X_train_std, X_test_std = X_train, X_test
 
             if model_type == 'lr':
                 yhat, model_ = build_lr(X_train_std, y_train, X_test_std)
@@ -197,7 +221,8 @@ def model_evaluation(true, pred):
     rmse =  np.sqrt(mse)
     mape = np.mean(np.abs((true2 - pred2) / np.abs(true2)))
     acc = (1 - mape)
-
+    
+    print("\tr^2=%f"%metrics.r2_score(true2,pred2))
     print('Mean Absolute Error (MAE):', mae)
     print('Mean Squared Error (MSE):', mse)
     print('Root Mean Squared Error (RMSE):', rmse)
@@ -206,3 +231,47 @@ def model_evaluation(true, pred):
 
     return mae, mse, rmse, mape, acc
 # %%
+def load_data(m_data):
+    m_data=m_data[['Timestamp','Day_Ahead_Prices', 'Actual Load', 'DK_1', 'SE_4', 'DK_1_Exports_Volume','SE_4_Exports_Volume', 'DE_LU_corr', 'DE_LU_Exports_Volume_corr', 'Solar','Wind Total',"CO2_prices_Price","TTF=F_Price","CFI2Z3_Price","Hour","Weekday","business"]]
+    #m_data=m_data[['Timestamp','Day-ahead prices', 'Actual Load', 'Solar','Wind Total',"TTF","CO2","Hour","Day","business"]]
+    return m_data
+    
+def create_time(data,days):
+    sdate = data.min()
+    edate = data.max() + pd.DateOffset(days)
+    dt=pd.date_range(sdate,edate,freq='H')
+    dl = pd.DataFrame(data=None)
+    dl["Timestamp"]=dt
+    return dl
+
+def lagging(f1):
+    cols=[ 'Actual Load', 'DK_1', 'SE_4', 'DK_1_Exports_Volume','SE_4_Exports_Volume', 'DE_LU_corr', 'DE_LU_Exports_Volume_corr', 'Solar','Wind Total']
+    for i in cols:
+        f1[i+"_mean4"]=f1[i].shift(1, axis = 0).rolling(4,min_periods=1).mean().round(2)
+        f1=f1.drop(i,axis=1)
+    return f1
+
+
+def split(datam):
+    time = create_time(datam['Timestamp'],days=14)
+    datam =time.merge(datam, how='left', on='Timestamp')
+    datam["Weekday"]=datam["Timestamp"].dt.weekday
+    datam["Hour"]=datam["Timestamp"].dt.hour
+    datam=datam.drop_duplicates('Timestamp', keep='last')  
+    
+    dn = dict(tuple(datam.groupby('Weekday')))
+    df0 = pd.DataFrame(data=None)
+    for j in range(7):
+        dn[j]=dict(tuple(dn[j].groupby('Hour')))
+        for i in range(24):
+            dn[j][i]=lagging(dn[j][i])
+            if(i==0 and j==1):
+                df0 =time.merge(dn[j][i], how='inner', on='Timestamp')
+            else:
+                df0=df0.append(dn[j][i], ignore_index=True)
+    df0=df0.sort_values("Timestamp").reset_index(drop=True)
+    cols=["CO2_prices_Price","TTF=F_Price","CFI2Z3_Price"]
+    for i in cols:
+        df0[i]=df0[i].fillna(method='pad')
+    out_data=df0
+    return out_data
