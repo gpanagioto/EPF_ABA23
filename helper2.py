40043a3acbaff0dbf93fed33c6948deb728b51f9
DimSalogiannis <mitsoss.salo@gmail.com>
2023-04-23 14:12:08 +0200

    feature importances for K=0

--- helper2.py
+++ helper2.py
@@ -14,7 +14,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import SelectFromModel
 from operator import itemgetter
 
-def 
 
 # From Fran
 def imp_df(column_names, importances):
@@ -63,9 +62,9 @@ def lin_reg_feat_importance(x_train, y_train, x_test, y_test):
     return lr_imp
 
 
-
-def random_f_2(df, x_train, y_train, x_test, y_test):
     
+def build_rf_2(x_train, y_train, x_test, random_search = False):
+    from sklearn.ensemble import RandomForestRegressor
     rf = RandomForestRegressor(random_state = 42, min_samples_split = 10)
     if random_search: # perform randomized search cross validation
         rf_grid = {'max_depth': [3, 7, 10],
@@ -78,53 +77,48 @@ def random_f_2(df, x_train, y_train, x_test, y_test):
     else:
         rf.fit(x_train, y_train)
         model_ = rf
-        
-    model_.fit(x_train, y_train)
     
-    print("R^2 Training Score:", model_.score(x_train, y_train))
-    print("R^2 OOB Score:", model_.oob_score_)
-    print("R^2 Test Score:", model_.score(x_test, y_test))
-    
-    pred_diff = abs(y_train - model_.predict(x_train))
-    min_error_ind = np.argmin(pred_diff)
-    max_error_ind = np.argmax(pred_diff)
-    print('Index with smallest error:', min_error_ind)
-    print('Index with largest error:', max_error_ind)
-    
-    selected_rows = [min_error_ind, max_error_ind]
-    selected_df = x_train.iloc[selected_rows, :].values
-    prediction, bias, contributions = ti.predict(model_, selected_df)
-    
-    for i in range(len(selected_rows)):
-        print("Row", selected_rows[i])
-        print("Prediction:", prediction[i][0], 'Actual Value:', y_train[selected_rows[i]])
-        print("Bias (trainset mean)", bias[i])
-        print("Feature contributions:")
-        for c, feature in sorted(zip(contributions[i], x_train.columns), 
-                                key=lambda x: -abs(x[0])):
-            print(feature, round(c, 2))
-        print("-"*20) 
-        
-        
-    prediction1, bias1, contributions1 = ti.predict(model_, 
-                                                    np.array([selected_df[0]]), 
-                                                    joint_contribution=True)
-    prediction2, bias2, contributions2 = ti.predict(model_, 
-                                                    np.array([selected_df[1]]), 
-                                                    joint_contribution=True)
-    
-    aggregated_contributions1 = utils.aggregated_contribution(contributions1)
-    aggregated_contributions2 = utils.aggregated_contribution(contributions2)
-    
-    res = []
-    for k in set(aggregated_contributions1.keys()).union(
-                set(aggregated_contributions2.keys())):
-        res.append(([x_train.columns[index] for index in k], 
-                aggregated_contributions1.get(k, 0) \
-                    - aggregated_contributions2.get(k, 0)))   
-            
-    for lst, v in (sorted(res, key=lambda x:-abs(x[1])))[:10]:
-        print (lst, v)
-        
-    return model_
+    feature_importances = model_.feature_importances_
+    feature_names = list(x_train.columns)
+    imp_df_ = imp_df(feature_names, feature_importances)
+    var_imp_plot(imp_df_, "Feature importance for Random Forest")
 
+    ypred = model_.predict(x_test)
+    params = model_.get_params()
+    
+    return ypred, params
+
+
+def gamw(x_train, y_train, random_search=False):
+    from sklearn.ensemble import RandomForestRegressor
+    from sklearn.model_selection import RandomizedSearchCV
+    rf = RandomForestRegressor(random_state=42, min_samples_split=10)
+    if random_search:
+        rf_grid = {
+            'max_depth': [3, 7, 10],
+            'min_samples_split': np.arange(2, 30, 2),
+            'min_samples_leaf': np.arange(2, 15, 2)
+        }
+        rf = RandomizedSearchCV(
+            estimator=rf, param_distributions=rf_grid, n_iter=30, cv=3, random_state=42, n_jobs=-1
+        )
+        rf.fit(x_train, y_train)
+        model_ = rf.best_estimator_
+    else:
+        rf.fit(x_train, y_train)
+        model_ = rf
+        
+    importances = model_.feature_importances_
+    return importances
+
+
+
+def xgb_feat_imp(x_train, y_train):
+    
+    import xgboost as xgb
+    
+    xg_reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 10)
+    xg_reg.fit(x_train,y_train)
+    importances = xg_reg.feature_importances_
+    return importances
