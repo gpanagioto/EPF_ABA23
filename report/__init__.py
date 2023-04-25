075a9778667f935304fac1118d1716dae72980cd
DimSalogiannis <mitsoss.salo@gmail.com>
2023-04-25 19:28:38 +0200

    report folder added

---
+++ report/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+# Get the parent directory of the package directory (i.e. myproject directory)
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+data_path = os.path.join('..',root_dir,'dataset_management','data')
