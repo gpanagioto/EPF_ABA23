9bc131583f07c49754f92edd686405300189f78c
gpanagioto <ge.panagioto@gmail.com>
2023-04-25 04:34:58 +0200

    merging with features importance

--- models/helper.py
+++ models/helper.py
@@ -1,77 +1,59 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
 
 from sklearn import metrics
-from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor
-from sklearn.model_selection import RandomizedSearchCV, TimeSeriesSplit, train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV, TimeSeriesSplit
 from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPRegressor 
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 import datetime as dt
 import seaborn as sns
 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-
 import xgboost as xg
 
 #%%
-# def read_our_data(file_name:str) -> pd.DataFrame():
-#     file_dir = './data/'+file_name
-#     df = pd.read_csv(file_dir)
-#     cols_check = ['Timestamp', 'Date']
-#     for col in cols_check:
-#         if col in df.columns:
-#             df[col] = pd.to_datetime(df[col])
-#     return df
+def read_our_data(file_name):
+    file_dir = './dataset_management/data/clean/'+file_name
+    df = pd.read_csv(file_dir)
+    cols_check = ['Timestamp', 'Date']
+    for col in cols_check:
+        if col in df.columns:
+            df[col] = pd.to_datetime(df[col])
+    return df
 
-# def lag_df(df:pd.DataFrame(), col:str, lags:list) -> pd.DataFrame():
-#     df_lagged = df.copy()
-#     for lag in lags:
-#         new_col = str(col) + '-lag' + str(lag)
-#         df_lagged[new_col] = df_lagged[col].shift(lag)
-    
-#     return df_lagged
-
-
-def drop_lag_df(df:pd.DataFrame(), new_col:str, lags:list) -> pd.DataFrame():
+def lag_df(df, col, lags):
     df_lagged = df.copy()
     for lag in lags:
-        col = str(new_col) + '-lag' + str(lag)
-        print(col, df_lagged[df_lagged[col].isna()]['Date'].unique())
-        idx_drop=(df_lagged[df_lagged[col].isna()]['Date'].unique())
-        df_lagged.drop(df_lagged[df_lagged['Date'].isin(idx_drop)].index, inplace = True)
+        new_col = str(col) + '-lag' + str(lag)
+        df_lagged[new_col] = df[col].shift(lag)
     return df_lagged
 
-
-def split_timeseries(df:pd.DataFrame(), train_start:list, cnt:int, method:int, perc = 0.85) -> pd.DataFrame(): # train_start list of dates; formatted YYYY-mm-dd
+def split_timeseries(df, train_start, cnt, method, perc = 0.85): # train_start array of dates; formatted YYYY-mm-dd
     # method {0 - moving blocks, 1 - from the begging}
-    df.head()
     if 'Timestamp' in df.columns:
         df.set_index('Timestamp', inplace = True)
 
     if cnt == len(train_start)-1:
-        end=pd.to_datetime(df['Date'].max()) + dt.timedelta(hours = 23)
-
+        end=df['Date'].max()
     else:
-        end = train_start[cnt +1] - dt.timedelta(hours = 1)
+        end = train_start[cnt +1] - dt.timedelta(days=1)
 
     if method == 0:
         start = train_start[cnt]
         delta=(end-start).days
-        train_end = start + pd.DateOffset(days=round(perc*delta,0), hours = 23)
-        test_start = train_end + pd.DateOffset(hours = 1)
+        train_end = start + pd.DateOffset(days=round(perc*delta,0))
+        test_start = train_end + pd.DateOffset(days=1)
 
     elif method == 1:
         start = train_start[0]
         delta=(end-start).days
-        train_end = end - pd.DateOffset(days = 30)
-        test_start = train_end + pd.DateOffset(hours = 1)
+        train_end = end - pd.DateOffset(days=91)
+        test_start = train_end + pd.DateOffset(days=1)
     
-    print(f'train {start} - {train_end}, test {test_start} - {end}')
+
     trainset = df.loc[start:train_end]
     testset = df.loc[test_start:end]
        
@@ -99,9 +81,9 @@ def standardize(train_set, test_set, cols):
 
 #%%
 def build_lr(x_train, y_train, x_test): # CONSIDER USING STATSTOOLS.OLS AS IT PROVIDES TTEST
-    model_ = LinearRegression()
-    model_.fit(x_train, y_train)
-
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    model_ = lr
     ypred = model_.predict(x_test)
     params = model_.get_params()
 
@@ -126,12 +108,13 @@ def build_rf(x_train, y_train, x_test, random_search = False):
         rf_grid = {'max_depth': [3, 7, 10],
                    'min_samples_split': np.arange(2, 30, 2),
                    'min_samples_leaf': np.arange(2, 15, 2)}
-        model_ = RandomizedSearchCV(estimator = rf, param_distributions = rf_grid, n_iter = 30, cv = 3, random_state = 42, n_jobs = -1)
-        model_.fit(x_train, y_train)
-        model_.best_estimator_.get_params() # optimal parameters
-        model_ = model_.best_estimator_ # evaluate optimal model
+        rf = RandomizedSearchCV(estimator = rf, param_distributions = rf_grid, n_iter = 30, cv = 3, random_state = 42, n_jobs = -1)
+        rf.fit(x_train, y_train)
+        rf.best_estimator_.get_params() # optimal parameters
+        model_ = rf.best_estimator_ # evaluate optimal model
     else:
-        model_.fit(x_train, y_train)
+        rf.fit(x_train, y_train)
+        model_ = rf
         
     ypred = model_.predict(x_test)
     params = model_.get_params()
@@ -139,99 +122,71 @@ def build_rf(x_train, y_train, x_test, random_search = False):
     return ypred, params
 
 def build_xgb(x_train, y_train, x_test):
-    model_ = xg.XGBRegressor(seed = 42)
-    model_.fit(x_train, y_train)
-
-    ypred = model_.predict(x_test)
-
-    return ypred, model_
-
-def build_gb(x_train, y_train, x_test):
-    model_ = GradientBoostingRegressor(random_state = 42)
-    model_.fit(x_train, y_train)
+    xgb = xg.XGBRegressor(seed = 42)
+    xgb.fit(x_train, y_train)
     
+    model_= xgb
     ypred = model_.predict(x_test)
+    params = model_.params()
 
+    return ypred, params
+
+def nn (x_train, y_train, x_test, y_test):
+
+    model_ = nn
+    ypred = model_.predict(x_test)
     return ypred, model_
 
-def build_lstm (x_dev, y_dev, x_test):
+def run_model(model_type, df, k_folds, split_method, train_start, features, target, cols_std, pred_window = []): # argument timeframe is necessary to choose baseline
+    # model_type: lr (linear regression), ma (moving average), rf (random forest), xgb (XGBoost), nn (neural network)
 
-    tf.random.set_seed(42)
+    if model_type == 'ma':
+        ypred = build_ma(df, target, pred_window)
+        return ypred
 
-    x_train, x_val = train_test_split(x_dev,test_size = 0.2, shuffle=False)
-    y_train, y_val = train_test_split(y_dev,test_size = 0.2, shuffle=False)
+    else:
+        ypred = []
+        models = []
+        for k in range(k_folds):
+            # split in train and test set
+            train_set, test_set = split_timeseries(df, train_start, k, method = split_method)
 
-    # create and fit the LSTM network
-    model_ = keras.models.Sequential()
-    model_.add(layers.LSTM(64, input_shape = (x_train.shape[1], 1)))
-    model_.add(layers.Dense(1))
-    model_.compile(loss='mean_squared_error', optimizer='adam')
-    # change data type so it can be converted to a tensor
-    x_train = np.asarray(x_train).astype(np.float32)
-    x_val = np.asarray(x_val).astype(np.float32)
-    x_test = np.asarray(x_test).astype(np.float32)
-    model_.fit(x_train, y_train, epochs = 10, batch_size = 32, validation_data = (x_val, y_val))
+            # get features and target
+            X_train, y_train = get_feature_target(train_set, features, target)
+            X_test, y_test = get_feature_target(test_set, features, target)
 
-    ypred = model_.predict(x_test)
-    print(ypred.shape)
+            # standardize
+            X_train_std, X_test_std = standardize(X_train, X_test, cols_std)
 
-    return ypred, model_
+            if model_type == 'lr':
+                yhat, model_ = build_lr(X_train_std, y_train, X_test_std)
+            elif model_type == 'rf':
+                yhat, model_ = build_rf(X_train_std, y_train, X_test_std, random_search = True)
+            elif model_type == 'xgb':
+                yhat, model_ = build_xgb(X_train_std, y_train, X_test_std)
+            elif model_type == 'nn':
+                yhat, model_ = build_nn(X_train_std, y_train, X_test_std)
 
-
-def run_model(model_type, df, k_folds, split_method, train_start, features, target, cols_std, X_std = True): # argument timeframe is necessary to choose baseline
-    # model_type: lr (linear regression), rf (random forest), xgb (XGBoost), lstm (long-short term memory - recursive neural network)
-
-    ypred = []
-    models = []
-
-    for k in range(k_folds):
-        print('Iteration ', k)
-
-        # split in train and test set
-        train_set, test_set = split_timeseries(df, train_start, k, method = split_method)
-
-        # get features and target
-        X_train, y_train = get_feature_target(train_set, features, target)
-        X_test, y_test = get_feature_target(test_set, features, target)
-
-        # standardize
-        if X_std == True:
-            X_train, X_test = standardize(X_train, X_test, cols_std)
-
-        if model_type == 'lr':
-            yhat, model_ = build_lr(X_train, y_train, X_test)
-        elif model_type == 'rf':
-            yhat, model_ = build_rf(X_train, y_train, X_test, random_search = True)
-        elif model_type == 'xgb':
-            yhat, model_ = build_xgb(X_train, y_train, X_test)
-        elif model_type == 'gb':
-            yhat, model_ = build_gb(X_train, y_train, X_test)
-        #elif model_type == 'lstm':
-        #    yhat, model_ = build_lstm(X_train, y_train, X_test)
-        #    yhat = yhat.reshape(-1)
-        elif model_type == 'lstm_new':
-            yhat, model_ = build_lstm_new(X_train, y_train, X_test)
-            yhat = yhat.reshape(-1)
-      
-        
-        model_evaluation(y_test, yhat)
-        
-        ypred.append(yhat)
-        models.append(model_)
+            print('Iteration ', k)
+            model_evaluation(yhat, y_test)
             
-    return ypred, models
+            ypred.append(yhat)
+            models.append(model_)
+            
+        return ypred, models
 
 def model_evaluation(true, pred):
     idx = np.where(true != 0) # avoid dividing by 0 in mape calculation
     true2 = true[idx]
     pred2 = pred[idx]
+
     plt.plot(true2, label = 'True')
     plt.plot(pred2, label = 'Predicted')
     plt.title('True data vs prediction')
     plt.legend()
     plt.show()
     
-    corr = np.corrcoef(true2, pred2)
+    corr = np.corrcoef(true2, pred)
     sns.heatmap(corr, annot=True)
     plt.title('Correlation true data - prediction')
     plt.show()
@@ -241,8 +196,7 @@ def model_evaluation(true, pred):
     rmse =  np.sqrt(mse)
     mape = np.mean(np.abs((true2 - pred2) / np.abs(true2)))
     acc = (1 - mape)
-    
-    print("\tr^2=%f"%metrics.r2_score(true2,pred2))
+
     print('Mean Absolute Error (MAE):', mae)
     print('Mean Squared Error (MSE):', mse)
     print('Root Mean Squared Error (RMSE):', rmse)
@@ -251,47 +205,112 @@ def model_evaluation(true, pred):
 
     return mae, mse, rmse, mape, acc
 # %%
-def load_data(m_data):
-    m_data=m_data[['Timestamp','Day_Ahead_price', 'DK_1_imports', 'SE_4_imports', 'DK_1_exports','SE_4_exports', 'Actual_Load','Solar_[MW]', 'ttf_price', 'coal_price', 'co2_price','DE_LU_AT_imports', 'DE_LU_AT_exports', 'Wind Total',"Hour","Weekday","business"]]
-    #m_data=m_data[['Timestamp','Day-ahead prices', 'Actual Load', 'Solar','Wind Total',"TTF","CO2","Hour","Day","business"]]
-    return m_data
+
+
+
+################### Features Importance
+
+# From Fran
+def imp_df(column_names, importances):
+    """Function for creating a feature importance dataframe"""
+    df = pd.DataFrame({'feature': column_names, 'feature_importance': importances})\
+           .sort_values('feature_importance', ascending=False)\
+           .reset_index(drop=True)
+    return df
+
+# From Fran
+def var_imp_plot(imp_df, title):
+    """Plotting a feature importance dataframe (horizontal barchart)"""
+    sns.barplot(x='feature_importance', y='feature', data=imp_df, 
+                orient='h', color='mediumpurple')\
+       .set_title(title, fontsize = 20)
+
+def lin_reg_feat_importance(x_train, y_train, x_test, y_test):
     
-def create_time(data,days):
-    sdate = data.min()
-    edate = data.max() + pd.DateOffset(days)
-    dt=pd.date_range(sdate,edate,freq='H')
-    dl = pd.DataFrame(data=None)
-    dl["Timestamp"]=dt
-    return dl
+    # Scaling the data
+    model_lr = Pipeline([
+        ("scaler", StandardScaler()),
+        ("linear_regression", LinearRegression())
+    ])
 
-def lagging(f1):
-    cols=['DK_1_imports', 'SE_4_imports', 'DK_1_exports','SE_4_exports', 'Actual_Load','Solar_[MW]', 'DE_LU_AT_imports', 'DE_LU_AT_exports', 'Wind Total']
-    for i in cols:
-        f1[i+"_mean4"]=f1[i].shift(1, axis = 0).rolling(4,min_periods=1).mean().round(2)
-        f1=f1.drop(i,axis=1)
-    return f1
+    # R^2 score calculation
+    model_lr.fit(x_train, y_train)
+    model_lr.score(x_test, y_test)
+    lr_imp = imp_df(x_train.columns, model_lr["linear_regression"].coef_)
 
+    return lr_imp
 
-def split(datam):
-    time = create_time(datam['Timestamp'],days=14)
-    datam =time.merge(datam, how='left', on='Timestamp')
-    datam["Weekday"]=datam["Timestamp"].dt.weekday
-    datam["Hour"]=datam["Timestamp"].dt.hour
-    datam=datam.drop_duplicates('Timestamp', keep='last')  
+def build_rf_2(x_train, y_train, x_test, random_search = False):
+    from sklearn.ensemble import RandomForestRegressor
+    rf = RandomForestRegressor(random_state = 42, min_samples_split = 10)
+    if random_search: # perform randomized search cross validation
+        rf_grid = {'max_depth': [3, 7, 10],
+                   'min_samples_split': np.arange(2, 30, 2),
+                   'min_samples_leaf': np.arange(2, 15, 2)}
+        rf = RandomizedSearchCV(estimator = rf, param_distributions = rf_grid, n_iter = 30, cv = 3, random_state = 42, n_jobs = -1)
+        rf.fit(x_train, y_train)
+        rf.best_estimator_.get_params() # optimal parameters
+        model_ = rf.best_estimator_ # evaluate optimal model
+    else:
+        rf.fit(x_train, y_train)
+        model_ = rf
     
-    dn = dict(tuple(datam.groupby('Weekday')))
-    df0 = pd.DataFrame(data=None)
-    for j in range(7):
-        dn[j]=dict(tuple(dn[j].groupby('Hour')))
-        for i in range(24):
-            dn[j][i]=lagging(dn[j][i])
-            if(i==0 and j==1):
-                df0 =time.merge(dn[j][i], how='inner', on='Timestamp')
-            else:
-                df0=df0.append(dn[j][i], ignore_index=True)
-    df0=df0.sort_values("Timestamp").reset_index(drop=True)
-    cols=["co2_price","ttf_price","coal_price"]
-    for i in cols:
-        df0[i]=df0[i].fillna(method='pad')
-    out_data=df0
-    return out_data
+    feature_importances = model_.feature_importances_
+    feature_names = list(x_train.columns)
+    imp_df_ = imp_df(feature_names, feature_importances)
+    var_imp_plot(imp_df_, "Feature importance for Random Forest")
+
+    ypred = model_.predict(x_test)
+    params = model_.get_params()
+    
+    return ypred, params
+
+
+def gamw(x_train, y_train, random_search=False):
+    from sklearn.ensemble import RandomForestRegressor
+    from sklearn.model_selection import RandomizedSearchCV
+    rf = RandomForestRegressor(random_state=42, min_samples_split=10)
+    if random_search:
+        rf_grid = {
+            'max_depth': [3, 7, 10],
+            'min_samples_split': np.arange(2, 30, 2),
+            'min_samples_leaf': np.arange(2, 15, 2)
+        }
+        rf = RandomizedSearchCV(
+            estimator=rf, param_distributions=rf_grid, n_iter=30, cv=3, random_state=42, n_jobs=-1
+        )
+        rf.fit(x_train, y_train)
+        model_ = rf.best_estimator_
+    else:
+        rf.fit(x_train, y_train)
+        model_ = rf
+        
+    importances = model_.feature_importances_
+    return importances
+
+
+
+def xgb_feat_imp(x_train, y_train):
+    
+    import xgboost as xgb
+    
+    xg_reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 10)
+    xg_reg.fit(x_train,y_train)
+    importances = xg_reg.feature_importances_
+    return importances
+
+# def lin_reg_feat_importance(x_train, y_train, x_test, y_test):
+    
+#     # Scaling the data
+#     model_lr = Pipeline([
+#         ("scaler", StandardScaler()),
+#         ("linear_regression", LinearRegression())
+#     ])
+
+#     # R^2 score calculation
+#     model_lr.fit(x_train, y_train)
+#     model_lr.score(x_test, y_test)
+#     lr_imp = imp_df(df.columns, model_lr["linear_regression"].coef_)
+
+#     lr_imp
